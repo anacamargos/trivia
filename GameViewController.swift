292dcb37f22cadf6c90bb954110ef6fd82f6d1c4
Ana <alcvlara@sga.pucminas.br>
2019-04-06 19:00:49 -0300

    Updating files

--- GameViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GameViewController.swift
-//  Trivia
-//
-//  Created by Ana Leticia Camargos on 06/04/19.
-//  Copyright © 2019 Ana Leticia Camargos. All rights reserved.
-//
-
-import Foundation
